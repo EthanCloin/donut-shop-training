2ea5106f3cf417f37af9788c41e72f13831cf93e
Ethan Cloin <psnethan@gmail.com>
2022-10-12 21:19:39 -0400

    boilerplate for components

---
+++ src/components/Donut.tsx
@@ -0,0 +1,7 @@
+import "./Donut.css";
+
+const Donut = () => {
+  return <div className="Donut">Donut works</div>;
+};
+
+export default Donut;
---
+++ src/components/DonutList.tsx
@@ -0,0 +1,7 @@
+import "./DonutList.css";
+
+const DonutList = () => {
+  return <div className="DonutList">DonutList works</div>;
+};
+
+export default DonutList;
